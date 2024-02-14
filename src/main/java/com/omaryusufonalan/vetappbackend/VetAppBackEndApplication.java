910bb40fd875d91a7c8553dd2e949d4a006436c1
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-14 19:05:43 +0300

    Create workspace

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/VetAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.omaryusufonalan.vetappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetAppBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omaryusufonalan/vetappbackend/VetAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omaryusufonalan.vetappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
