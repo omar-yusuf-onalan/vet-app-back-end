865b0604cfa26b079a1bd632f625951331b9314f
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-14 19:42:34 +0300

    Create AnimalRequest.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/dto/animal/AnimalRequest.java
@@ -0,0 +1,25 @@
+package com.omaryusufonalan.vetappbackend.dto.animal;
+
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class AnimalRequest {
+    private String name;
+
+    private String species;
+
+    private String breed;
+
+    private String gender;
+
+    private String color;
+
+    private LocalDate dateOfBirth;
+}
