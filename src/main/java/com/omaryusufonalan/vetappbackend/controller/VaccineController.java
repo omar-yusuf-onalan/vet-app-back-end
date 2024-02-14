649f47d24f87d14ef4d311ec447fdbc9e1d18aec
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-14 20:06:38 +0300

    Create VaccineController.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/controller/VaccineController.java
@@ -0,0 +1,36 @@
+package com.omaryusufonalan.vetappbackend.controller;
+
+import com.omaryusufonalan.vetappbackend.dto.vaccine.VaccineRequest;
+import com.omaryusufonalan.vetappbackend.service.VaccineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("vet-app/api/v1/vaccine")
+@RequiredArgsConstructor
+public class VaccineController {
+    private final VaccineService vaccineService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getResponseById(@PathVariable Long id) {
+        return new ResponseEntity<>(vaccineService.getResponseById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody VaccineRequest vaccineRequest) {
+        return new ResponseEntity<>(vaccineService.create(vaccineRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody VaccineRequest vaccineRequest) {
+        return new ResponseEntity<>(vaccineService.update(id, vaccineRequest), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        vaccineService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
