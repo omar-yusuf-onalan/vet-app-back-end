6a50cb1880a65a1371482c49f81e07186d617a7e
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-14 20:58:58 +0300

    Create AppointmentResponse.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/dto/appointment/AppointmentResponse.java
@@ -0,0 +1,22 @@
+package com.omaryusufonalan.vetappbackend.dto.appointment;
+
+import com.omaryusufonalan.vetappbackend.dto.animal.AnimalWithoutAppointmentResponse;
+import com.omaryusufonalan.vetappbackend.dto.doctor.DoctorWithoutAppointmentResponse;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class AppointmentResponse {
+    private Long id;
+
+    private LocalDateTime appointmentDate;
+
+    private AnimalWithoutAppointmentResponse animal;
+
+    private DoctorWithoutAppointmentResponse doctor;
+}
