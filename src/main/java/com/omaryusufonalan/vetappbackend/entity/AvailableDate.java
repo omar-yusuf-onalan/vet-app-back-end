c60a8369658ce01dca2b3d72211148374dbffe2a
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-14 20:37:08 +0300

    Create AvailableDate.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/entity/AvailableDate.java
@@ -0,0 +1,28 @@
+package com.omaryusufonalan.vetappbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AvailableDate {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDate availableDate;
+
+    @ManyToOne
+    private Doctor doctor;
+}
