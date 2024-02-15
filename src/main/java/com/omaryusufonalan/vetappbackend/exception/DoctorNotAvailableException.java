4d3b51fbec6ff1f17ce5afff6ad63a281a1a46eb
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-15 10:24:44 +0300

    Create DoctorNotAvailableException.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/exception/DoctorNotAvailableException.java
@@ -0,0 +1,7 @@
+package com.omaryusufonalan.vetappbackend.exception;
+
+public class DoctorNotAvailableException extends RuntimeException {
+    public DoctorNotAvailableException(String message) {
+        super(message);
+    }
+}
