9d2576cde93c5acc4127329aa565aa594e79bcb3
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-15 10:14:56 +0300

    Add checkForConflictingAppointmentHours method

--- src/main/java/com/omaryusufonalan/vetappbackend/repository/AppointmentRepository.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/repository/AppointmentRepository.java
@@ -2,8 +2,14 @@ package com.omaryusufonalan.vetappbackend.repository;
 
 import com.omaryusufonalan.vetappbackend.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    @Query(value = "SELECT * FROM appointments WHERE appointment_date = ?1", nativeQuery = true)
+    Optional<Appointment> checkForConflictingAppointmentHours(LocalDateTime appointment);
 }
