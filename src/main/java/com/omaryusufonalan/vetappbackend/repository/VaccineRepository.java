3a97a35d1e5a9954028955dbdd29a335a2097a5d
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-15 09:59:27 +0300

    Add checkForVaccineInEffect method

--- src/main/java/com/omaryusufonalan/vetappbackend/repository/VaccineRepository.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/repository/VaccineRepository.java
@@ -2,8 +2,16 @@ package com.omaryusufonalan.vetappbackend.repository;
 
 import com.omaryusufonalan.vetappbackend.entity.Vaccine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 public interface VaccineRepository extends JpaRepository<Vaccine, Long> {
+    @Query(value = "SELECT * FROM vaccines WHERE vaccine_code = ?1 AND vaccine_animal_id = ?2" +
+            " AND vaccine_protection_finish_date > ?3",
+            nativeQuery = true)
+    Optional<Vaccine> checkForVaccineInEffect(String code, Long animalId, LocalDate protectionStartDate);
 }
