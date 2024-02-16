f49a1850859263fdb3df8966cd7c124d2b8d4285
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-16 12:01:02 +0300

    Implement getPageResponse method

--- src/main/java/com/omaryusufonalan/vetappbackend/service/AvailableDateService.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/service/AvailableDateService.java
@@ -2,11 +2,17 @@ package com.omaryusufonalan.vetappbackend.service;
 
 import com.omaryusufonalan.vetappbackend.dto.availabledate.AvailableDateRequest;
 import com.omaryusufonalan.vetappbackend.dto.availabledate.AvailableDateResponse;
+import com.omaryusufonalan.vetappbackend.dto.availabledate.AvailableDateResponse;
+import com.omaryusufonalan.vetappbackend.dto.page.PageResponse;
+import com.omaryusufonalan.vetappbackend.entity.AvailableDate;
 import com.omaryusufonalan.vetappbackend.entity.AvailableDate;
 import com.omaryusufonalan.vetappbackend.mapper.AvailableDateMapper;
 import com.omaryusufonalan.vetappbackend.repository.AvailableDateRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -17,6 +23,7 @@ import java.util.Optional;
 public class AvailableDateService implements GenericCRUD<AvailableDate, AvailableDateRequest, AvailableDateResponse> {
     private final AvailableDateRepository availableDateRepository;
     private final AvailableDateMapper availableDateMapper;
+    private final PageService<AvailableDateResponse> pageService;
 
     @Override
     public AvailableDate getById(Long id) {
@@ -29,6 +36,18 @@ public class AvailableDateService implements GenericCRUD<AvailableDate, Availabl
         AvailableDate availableDateRetrievedFromDatabase = getById(id);
 
         return availableDateMapper.asAvailableDateResponse(availableDateRetrievedFromDatabase);
+    }
+
+    @Override
+    public PageResponse<AvailableDateResponse> getPageResponse(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+
+        Page<AvailableDate> pageOfAvailableDates = availableDateRepository.findAll(pageable);
+
+        Page<AvailableDateResponse> pageOfAvailableDateResponses = pageOfAvailableDates
+                .map(availableDate -> availableDateMapper.asAvailableDateResponse(availableDate));
+
+        return pageService.createPageResponse(page, pageSize, pageOfAvailableDateResponses);
     }
 
     @Override
