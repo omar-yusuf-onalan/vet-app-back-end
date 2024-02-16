14619f55a567f28c2de50f1e1b8e3321a2eac2f8
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-16 12:02:22 +0300

    Implement getPageResponse method

--- src/main/java/com/omaryusufonalan/vetappbackend/service/DoctorService.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/service/DoctorService.java
@@ -1,12 +1,18 @@
 package com.omaryusufonalan.vetappbackend.service;
 
+import com.omaryusufonalan.vetappbackend.dto.doctor.DoctorResponse;
 import com.omaryusufonalan.vetappbackend.dto.doctor.DoctorRequest;
 import com.omaryusufonalan.vetappbackend.dto.doctor.DoctorResponse;
+import com.omaryusufonalan.vetappbackend.dto.page.PageResponse;
+import com.omaryusufonalan.vetappbackend.entity.Doctor;
 import com.omaryusufonalan.vetappbackend.entity.Doctor;
 import com.omaryusufonalan.vetappbackend.mapper.DoctorMapper;
 import com.omaryusufonalan.vetappbackend.repository.DoctorRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +20,7 @@ import org.springframework.stereotype.Service;
 public class DoctorService implements GenericCRUD<Doctor, DoctorRequest, DoctorResponse> {
     private final DoctorRepository doctorRepository;
     private final DoctorMapper doctorMapper;
+    private final PageService<DoctorResponse> pageService;
 
     @Override
     public Doctor getById(Long id) {
@@ -26,6 +33,18 @@ public class DoctorService implements GenericCRUD<Doctor, DoctorRequest, DoctorR
         Doctor doctorRetrievedFromDatabase = getById(id);
 
         return doctorMapper.asDoctorResponse(doctorRetrievedFromDatabase);
+    }
+
+    @Override
+    public PageResponse<DoctorResponse> getPageResponse(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+
+        Page<Doctor> pageOfDoctors = doctorRepository.findAll(pageable);
+
+        Page<DoctorResponse> pageOfDoctorResponses = pageOfDoctors
+                .map(doctor -> doctorMapper.asDoctorResponse(doctor));
+
+        return pageService.createPageResponse(page, pageSize, pageOfDoctorResponses);
     }
 
     @Override
