7ec8baa1720a387e10fb4a097b7776cc14ba6823
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-16 13:46:41 +0300

    Add findByNameIgnoreCaseContaining method

--- src/main/java/com/omaryusufonalan/vetappbackend/repository/AnimalRepository.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/repository/AnimalRepository.java
@@ -1,9 +1,11 @@
 package com.omaryusufonalan.vetappbackend.repository;
 
 import com.omaryusufonalan.vetappbackend.entity.Animal;
+import com.omaryusufonalan.vetappbackend.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
+    Customer findByNameIgnoreCaseContaining(String name);
 }
