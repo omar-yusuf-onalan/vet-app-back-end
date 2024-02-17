ae09c2735bc4bf58490dc25a5cc8a8982c1fbbfb
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-18 02:16:52 +0300

    Change method signature

--- src/main/java/com/omaryusufonalan/vetappbackend/service/GetOwnedAnimals.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/service/GetOwnedAnimals.java
@@ -1,6 +1,6 @@
 package com.omaryusufonalan.vetappbackend.service;
 
-import com.omaryusufonalan.vetappbackend.dto.animal.AnimalResponse;
+import com.omaryusufonalan.vetappbackend.dto.animal.AnimalWithoutCustomerResponse;
 import com.omaryusufonalan.vetappbackend.entity.Animal;
 
 import java.util.Set;
@@ -8,5 +8,5 @@ import java.util.Set;
 public interface GetOwnedAnimals {
     Set<Animal> getAllOwnedAnimals(Long id);
 
-    Set<AnimalResponse> getAllOwnedAnimalResponses(Long id);
+    Set<AnimalWithoutCustomerResponse> getAllOwnedAnimalWithoutCustomerResponses(Long id);
 }
