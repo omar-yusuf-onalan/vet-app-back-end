d0587221edb9e2a5a86ea159f219df7e3acb7209
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-01 23:13:15 +0300

    Create Animal.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/entity/Animal.java
@@ -0,0 +1,34 @@
+package com.omaryusufonalan.vetappbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Animal {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String species;
+
+    private String breed;
+
+    private String gender;
+
+    private String color;
+
+    private LocalDate dateOfBirth;
+}
