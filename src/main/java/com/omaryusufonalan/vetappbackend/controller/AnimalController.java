2ae1baa6042062a8f8101a22deb0ca5526db9d2f
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-01 23:27:16 +0300

    Create AnimalController.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/controller/AnimalController.java
@@ -0,0 +1,40 @@
+package com.omaryusufonalan.vetappbackend.controller;
+
+import com.omaryusufonalan.vetappbackend.dto.animal.AnimalCreateRequest;
+import com.omaryusufonalan.vetappbackend.dto.animal.AnimalUpdateRequest;
+import com.omaryusufonalan.vetappbackend.service.animal.AnimalService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static com.omaryusufonalan.vetappbackend.config.BaseURL.BASE_URL;
+
+@RestController
+@RequestMapping(BASE_URL + "/animals")
+@RequiredArgsConstructor
+public class AnimalController {
+    private final AnimalService animalService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(animalService.getAnimalById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody AnimalCreateRequest animalSaveRequest) {
+        return new ResponseEntity<>(animalService.createAnimal(animalSaveRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@Valid @RequestBody AnimalUpdateRequest animalUpdateRequest){
+        return new ResponseEntity<>(animalService.updateAnimal(animalUpdateRequest),HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        animalService.deleteAnimalById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
