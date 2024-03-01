38be7f622031d6f3757343ac083c41c871af0094
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-01 22:33:02 +0300

    Create CustomerRepository.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.omaryusufonalan.vetappbackend.repository;
+
+import com.omaryusufonalan.vetappbackend.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
