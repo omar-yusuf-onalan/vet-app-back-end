720a6f9bb9cca276c71ddd7b5e16980892600f37
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-01 22:20:24 +0300

    Clear project

--- src/main/java/com/omaryusufonalan/vetappbackend/service/interfaces/FilterOperation.java
+++
@@ -1,7 +0,0 @@
-package com.omaryusufonalan.vetappbackend.service.interfaces;
-
-public interface FilterOperation<Entity, EntityResponse> {
-    Entity filterByName(String name);
-
-    EntityResponse filterResponseByName(String name);
-}
