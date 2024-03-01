8871c57c83ea003924882e8645b4aca7de33ecda
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-01 23:26:22 +0300

    Create FilterAnimal.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/service/animal/FilterAnimal.java
@@ -0,0 +1,11 @@
+package com.omaryusufonalan.vetappbackend.service.animal;
+
+import com.omaryusufonalan.vetappbackend.dto.animal.AnimalResponse;
+import com.omaryusufonalan.vetappbackend.entity.Animal;
+
+import java.util.List;
+
+public interface FilterAnimal {
+    List<Animal> filterAnimalsByName(String animalName);
+    List<AnimalResponse> filterAnimalResponsesByName(String animalName);
+}
