c82e9600ec942fca5c1a4ebe481359bba8032103
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-01 23:14:42 +0300

    Create OneToMany relation between Customer and Animal

--- src/main/java/com/omaryusufonalan/vetappbackend/entity/Animal.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/entity/Animal.java
@@ -3,6 +3,7 @@ package com.omaryusufonalan.vetappbackend.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,4 +32,7 @@ public class Animal {
     private String color;
 
     private LocalDate dateOfBirth;
+
+    @ManyToOne
+    private Customer customer;
 }
--- src/main/java/com/omaryusufonalan/vetappbackend/entity/Customer.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/entity/Customer.java
@@ -1,13 +1,12 @@
 package com.omaryusufonalan.vetappbackend.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -30,4 +29,7 @@ public class Customer {
     private String address;
 
     private String city;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "customer", cascade = CascadeType.REMOVE)
+    private List<Animal> animals;
 }
