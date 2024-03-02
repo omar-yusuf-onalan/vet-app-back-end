31e16da7d3ac4f561383d17e52d9b68c34a89f94
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 18:52:39 +0300

    Add availableDates field

--- src/main/java/com/omaryusufonalan/vetappbackend/dto/doctor/DoctorResponse.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/dto/doctor/DoctorResponse.java
@@ -1,9 +1,12 @@
 package com.omaryusufonalan.vetappbackend.dto.doctor;
 
+import com.omaryusufonalan.vetappbackend.dto.availabledate.OnlyAvailableDateResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,4 +24,6 @@ public class DoctorResponse {
     private String address;
 
     private String city;
+
+    private List<OnlyAvailableDateResponse> availableDates;
 }
