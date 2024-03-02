e3ee3571095c403283fc326c73b80c1b0dcdcb58
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 21:32:14 +0300

    Create Appointment.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/entity/Appointment.java
@@ -0,0 +1,24 @@
+package com.omaryusufonalan.vetappbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Appointment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDateTime appointmentDate;
+}
