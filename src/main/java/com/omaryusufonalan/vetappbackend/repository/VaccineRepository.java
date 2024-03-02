259c89ca4837c722d107c6e349cb6790ef7f2375
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 17:58:31 +0300

    Add findByAnimalId method

--- src/main/java/com/omaryusufonalan/vetappbackend/repository/VaccineRepository.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/repository/VaccineRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface VaccineRepository extends JpaRepository<Vaccine, Long> {
     @Query(value = "SELECT * FROM vaccine WHERE code = ?1 AND protection_finish_date > ?2", nativeQuery = true)
     Optional<Vaccine> validateVaccine(String code, LocalDate protectionStartDate);
+    List<Vaccine> findByAnimalId(Long animalId);
 }
