c6ad6baa65d820de27b050355fe2030e5f7723fa
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 18:45:37 +0300

    Create DoctorController.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/controller/DoctorController.java
@@ -0,0 +1,40 @@
+package com.omaryusufonalan.vetappbackend.controller;
+
+import com.omaryusufonalan.vetappbackend.dto.doctor.DoctorCreateRequest;
+import com.omaryusufonalan.vetappbackend.dto.doctor.DoctorUpdateRequest;
+import com.omaryusufonalan.vetappbackend.service.doctor.DoctorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static com.omaryusufonalan.vetappbackend.config.BaseURL.BASE_URL;
+
+@RestController
+@RequestMapping(BASE_URL + "/doctors")
+@RequiredArgsConstructor
+public class DoctorController {
+    private final DoctorService doctorService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(doctorService.getDoctorResponseById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody DoctorCreateRequest doctorSaveRequest) {
+        return new ResponseEntity<>(doctorService.createDoctor(doctorSaveRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@Valid @RequestBody DoctorUpdateRequest doctorUpdateRequest){
+        return new ResponseEntity<>(doctorService.updateDoctor(doctorUpdateRequest),HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        doctorService.deleteDoctorById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
