7cdc23b50dfc7b8002729d89f9354901f7f2e916
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 22:28:41 +0300

    Create FilterAvailableDate.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/service/availabledate/FilterAvailableDate.java
@@ -0,0 +1,16 @@
+package com.omaryusufonalan.vetappbackend.service.availabledate;
+
+import com.omaryusufonalan.vetappbackend.dto.availabledate.AvailableDateResponse;
+import com.omaryusufonalan.vetappbackend.entity.AvailableDate;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface FilterAvailableDate {
+    List<AvailableDate> filterAvailableDatesByDoctorIdAndAvailableDate(Long doctorId, LocalDate availableDate);
+    List<AvailableDateResponse> filterAvailableDateResponsesByDoctorIdAndAvailableDate(
+            Long doctorId,
+            LocalDate availableDate
+    );
+}
