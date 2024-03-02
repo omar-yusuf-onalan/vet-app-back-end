c920abc5dfb0c8574d7ae4c0137845cc86334dd0
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 22:13:20 +0300

    Create AppointmentService.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/service/appointment/AppointmentService.java
@@ -0,0 +1,50 @@
+package com.omaryusufonalan.vetappbackend.service.appointment;
+
+import com.omaryusufonalan.vetappbackend.dto.appointment.AppointmentCreateRequest;
+import com.omaryusufonalan.vetappbackend.dto.appointment.AppointmentResponse;
+import com.omaryusufonalan.vetappbackend.dto.appointment.AppointmentUpdateRequest;
+import com.omaryusufonalan.vetappbackend.entity.Appointment;
+import com.omaryusufonalan.vetappbackend.repository.AppointmentRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AppointmentService implements AppointmentCRUD {
+    private final AppointmentRepository appointmentRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public Appointment getAppointmentById(Long id) {
+        return appointmentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Appointment with ID " + id + " not found!"));
+    }
+
+    @Override
+    public AppointmentResponse getAppointmentResponseById(Long id) {
+        return modelMapper.map(getAppointmentById(id), AppointmentResponse.class);
+    }
+
+    @Override
+    public AppointmentResponse createAppointment(AppointmentCreateRequest appointmentCreateRequest) {
+        Appointment appointmentToBeCreated = modelMapper.map(appointmentCreateRequest, Appointment.class);
+
+        return modelMapper.map(appointmentRepository.save(appointmentToBeCreated), AppointmentResponse.class);
+    }
+
+    @Override
+    public AppointmentResponse updateAppointment(AppointmentUpdateRequest appointmentUpdateRequest) {
+        Appointment doesAppointmentExist = getAppointmentById(appointmentUpdateRequest.getId());
+
+        modelMapper.map(appointmentUpdateRequest, doesAppointmentExist);
+
+        return modelMapper.map(appointmentRepository.save(doesAppointmentExist), AppointmentResponse.class);
+    }
+
+    @Override
+    public void deleteAppointmentById(Long id) {
+        appointmentRepository.delete(getAppointmentById(id));
+    }
+}
