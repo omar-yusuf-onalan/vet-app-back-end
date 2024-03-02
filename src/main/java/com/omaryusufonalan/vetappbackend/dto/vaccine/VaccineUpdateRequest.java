3d84c901afb69be6eb6ebf24d6148efbc403b032
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 17:31:24 +0300

    Create VaccineUpdateRequest.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/dto/vaccine/VaccineUpdateRequest.java
@@ -0,0 +1,32 @@
+package com.omaryusufonalan.vetappbackend.dto.vaccine;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class VaccineUpdateRequest {
+    @Positive
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String code;
+
+    @NotNull
+    private LocalDate protectionStartDate;
+
+    @NotNull
+    private LocalDate protectionFinishDate;
+}
