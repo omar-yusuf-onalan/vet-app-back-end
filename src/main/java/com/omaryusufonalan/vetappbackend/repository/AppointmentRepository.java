01d029f5da11e7a692e9a56560a44a96669ed377
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 22:22:22 +0300

    Update AppointmentRepository.java

--- src/main/java/com/omaryusufonalan/vetappbackend/repository/AppointmentRepository.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/repository/AppointmentRepository.java
@@ -4,6 +4,10 @@ import com.omaryusufonalan.vetappbackend.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    Optional<Appointment> findByDoctorIdAndAppointmentDate(Long doctorId, LocalDateTime appointmentDate);
 }
