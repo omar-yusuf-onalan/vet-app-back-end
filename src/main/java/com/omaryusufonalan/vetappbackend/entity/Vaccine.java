d87a8b7b6c58d5236077cbf066f4900b456a0c49
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 17:25:50 +0300

    Create Vaccine.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/entity/Vaccine.java
@@ -0,0 +1,30 @@
+package com.omaryusufonalan.vetappbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Vaccine {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    private LocalDate protectionStartDate;
+
+    private LocalDate protectionFinishDate;
+}
