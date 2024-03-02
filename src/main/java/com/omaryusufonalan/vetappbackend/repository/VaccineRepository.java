b795bf6ac28f00323db349b1b511ff9040440d09
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 17:51:33 +0300

    Add validateVaccine method

--- src/main/java/com/omaryusufonalan/vetappbackend/repository/VaccineRepository.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/repository/VaccineRepository.java
@@ -2,8 +2,14 @@ package com.omaryusufonalan.vetappbackend.repository;
 
 import com.omaryusufonalan.vetappbackend.entity.Vaccine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 public interface VaccineRepository extends JpaRepository<Vaccine, Long> {
+    @Query(value = "SELECT * FROM vaccine WHERE code = ?1 AND protection_finish_date > ?2", nativeQuery = true)
+    Optional<Vaccine> validateVaccine(String code, LocalDate protectionStartDate);
 }
--- src/main/java/com/omaryusufonalan/vetappbackend/service/vaccine/VaccineService.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/service/vaccine/VaccineService.java
@@ -4,15 +4,18 @@ import com.omaryusufonalan.vetappbackend.dto.vaccine.VaccineCreateRequest;
 import com.omaryusufonalan.vetappbackend.dto.vaccine.VaccineResponse;
 import com.omaryusufonalan.vetappbackend.dto.vaccine.VaccineUpdateRequest;
 import com.omaryusufonalan.vetappbackend.entity.Vaccine;
+import com.omaryusufonalan.vetappbackend.exception.VaccineInEffectException;
 import com.omaryusufonalan.vetappbackend.repository.VaccineRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class VaccineService implements VaccineCRUD {
+public class VaccineService implements VaccineCRUD, ValidateVaccine {
     private final VaccineRepository vaccineRepository;
     private final ModelMapper modelMapper;
 
@@ -31,6 +34,8 @@ public class VaccineService implements VaccineCRUD {
     public VaccineResponse createVaccine(VaccineCreateRequest vaccineCreateRequest) {
         Vaccine vaccineToBeCreated = modelMapper.map(vaccineCreateRequest, Vaccine.class);
 
+        validateVaccine(vaccineToBeCreated);
+
         return modelMapper.map(vaccineRepository.save(vaccineToBeCreated), VaccineResponse.class);
     }
 
@@ -40,11 +45,22 @@ public class VaccineService implements VaccineCRUD {
 
         modelMapper.map(vaccineUpdateRequest, doesVaccineExist);
 
+        validateVaccine(doesVaccineExist);
+
         return modelMapper.map(vaccineRepository.save(doesVaccineExist), VaccineResponse.class);
     }
 
     @Override
     public void deleteVaccineById(Long id) {
         vaccineRepository.delete(getVaccineById(id));
+    }
+
+    @Override
+    public void validateVaccine(Vaccine vaccine) {
+        Optional<Vaccine> vaccineThatIsStillInEffect = vaccineRepository
+                .validateVaccine(vaccine.getCode(), vaccine.getProtectionStartDate());
+
+        if (vaccineThatIsStillInEffect.isPresent())
+            throw new VaccineInEffectException("Vaccine is still in effect!");
     }
 }
