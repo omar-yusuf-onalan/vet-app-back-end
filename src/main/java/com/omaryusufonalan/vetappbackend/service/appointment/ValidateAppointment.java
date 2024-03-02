c40e9832bebb3670562dfd0d52cbafe1ecb88de4
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 22:22:16 +0300

    Create ValidateAppointment.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/service/appointment/ValidateAppointment.java
@@ -0,0 +1,7 @@
+package com.omaryusufonalan.vetappbackend.service.appointment;
+
+import com.omaryusufonalan.vetappbackend.entity.Appointment;
+
+public interface ValidateAppointment {
+    void validateAppointment(Appointment appointment);
+}
