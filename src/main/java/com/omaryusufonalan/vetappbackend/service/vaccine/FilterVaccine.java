95d36397a71969e8c9cc8e7b58c015ca6e35a9ab
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 18:04:21 +0300

    Add findAllVaccinesBetweenTwoDates and findAllVaccineResponsesBetweenTwoDates methods

--- src/main/java/com/omaryusufonalan/vetappbackend/service/vaccine/FilterVaccine.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/service/vaccine/FilterVaccine.java
@@ -3,9 +3,12 @@ package com.omaryusufonalan.vetappbackend.service.vaccine;
 import com.omaryusufonalan.vetappbackend.dto.vaccine.VaccineResponse;
 import com.omaryusufonalan.vetappbackend.entity.Vaccine;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface FilterVaccine {
     List<Vaccine> filterVaccinesByAnimalId(Long animalId);
     List<VaccineResponse> filterVaccineResponsesByAnimalId(Long animalId);
+    List<Vaccine> findAllVaccinesBetweenTwoDates(LocalDate startDate, LocalDate finishDate);
+    List<VaccineResponse> findAllVaccineResponsesBetweenTwoDates(LocalDate startDate, LocalDate finishDate);
 }
