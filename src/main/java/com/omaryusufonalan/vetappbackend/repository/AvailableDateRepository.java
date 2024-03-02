3dd81169f4764730c90f4eaea5299b16fb929e29
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 22:28:27 +0300

    Change Optional to List

--- src/main/java/com/omaryusufonalan/vetappbackend/repository/AvailableDateRepository.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/repository/AvailableDateRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AvailableDateRepository extends JpaRepository<AvailableDate, Long> {
-    Optional<AvailableDate> findByDoctorIdAndAvailableDate(Long doctorId, LocalDate availableDate);
+    List<AvailableDate> findByDoctorIdAndAvailableDate(Long doctorId, LocalDate availableDate);
 }
