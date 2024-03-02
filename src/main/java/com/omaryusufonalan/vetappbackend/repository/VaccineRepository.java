54b833056fe0ed6f6a0cead99f4166a8bff26a76
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 17:32:45 +0300

    Create VaccineRepository.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/repository/VaccineRepository.java
@@ -0,0 +1,9 @@
+package com.omaryusufonalan.vetappbackend.repository;
+
+import com.omaryusufonalan.vetappbackend.entity.Vaccine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VaccineRepository extends JpaRepository<Vaccine, Long> {
+}
