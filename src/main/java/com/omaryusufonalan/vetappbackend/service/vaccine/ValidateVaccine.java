a277953640a63bf9123a1c099ac2d9b087d8caf1
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 17:51:02 +0300

    Create ValidateVaccine.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/service/vaccine/ValidateVaccine.java
@@ -0,0 +1,7 @@
+package com.omaryusufonalan.vetappbackend.service.vaccine;
+
+import com.omaryusufonalan.vetappbackend.entity.Vaccine;
+
+public interface ValidateVaccine {
+    void validateVaccine(Vaccine vaccine);
+}
