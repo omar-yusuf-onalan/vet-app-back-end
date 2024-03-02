4e1a486ccc60f71abd5dcf5a1a933afe28db1f34
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 23:08:23 +0300

    Add filterAppointmentResponsesByDoctorIdAndTwoDates and filterAppointmentResponsesByAnimalIdAndTwoDates methods

--- src/main/java/com/omaryusufonalan/vetappbackend/controller/AppointmentController.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/controller/AppointmentController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
 import static com.omaryusufonalan.vetappbackend.config.BaseURL.BASE_URL;
 
 @RestController
@@ -21,7 +23,33 @@ public class AppointmentController {
     public ResponseEntity<?> getById(@PathVariable("id") Long id){
         return new ResponseEntity<>(appointmentService.getAppointmentResponseById(id), HttpStatus.OK);
     }
-    
+
+    @GetMapping("/filter-by-doctor-id-and-two-dates/{doctorId}/{startDate}/{finishDate}")
+    public ResponseEntity<?> filterAppointmentResponsesByDoctorIdAndTwoDates(
+            @PathVariable("doctorId") Long doctorId,
+            @PathVariable("startDate") LocalDate startDate,
+            @PathVariable("finishDate") LocalDate finishDate
+    ){
+        return new ResponseEntity<>(appointmentService.filterAppointmentResponsesByDoctorIdAndTwoDates(
+                doctorId,
+                startDate,
+                finishDate
+        ), HttpStatus.OK);
+    }
+
+    @GetMapping("/filter-by-animal-id-and-two-dates/{doctorId}/{startDate}/{finishDate}")
+    public ResponseEntity<?> filterAppointmentResponsesByAnimalIdAndTwoDates(
+            @PathVariable("animalId") Long animalId,
+            @PathVariable("startDate") LocalDate startDate,
+            @PathVariable("finishDate") LocalDate finishDate
+    ){
+        return new ResponseEntity<>(appointmentService.filterAppointmentResponsesByAnimalIdAndTwoDates(
+                animalId,
+                startDate,
+                finishDate
+        ), HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<?> create(@Valid @RequestBody AppointmentCreateRequest appointmentSaveRequest) {
         return new ResponseEntity<>(appointmentService.createAppointment(appointmentSaveRequest), HttpStatus.CREATED);
