9628906afb29fe739de95aa2ae3bda25607e0bb7
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 18:52:07 +0300

    Create AvailableDateUpdateRequest.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/dto/availabledate/AvailableDateUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.omaryusufonalan.vetappbackend.dto.availabledate;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AvailableDateUpdateRequest {
+    @Positive
+    private Long id;
+
+    @NotNull
+    private LocalDate availableDate;
+}
