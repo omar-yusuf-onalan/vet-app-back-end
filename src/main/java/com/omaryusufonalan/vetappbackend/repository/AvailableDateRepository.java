8fc5b076218055c919f814dffcdc6b33db693c86
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-02 22:22:20 +0300

    Update AvailableDateRepository.java

--- src/main/java/com/omaryusufonalan/vetappbackend/repository/AvailableDateRepository.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/repository/AvailableDateRepository.java
@@ -2,8 +2,13 @@ package com.omaryusufonalan.vetappbackend.repository;
 
 import com.omaryusufonalan.vetappbackend.entity.AvailableDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 public interface AvailableDateRepository extends JpaRepository<AvailableDate, Long> {
+    Optional<AvailableDate> findByDoctorIdAndAvailableDate(Long doctorId, LocalDate availableDate);
 }
