1fc097764c2e3773724ac5a068937581b44b3745
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-03 13:28:00 +0300

    Create ReportUpdateRequest.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/dto/report/ReportUpdateRequest.java
@@ -0,0 +1,30 @@
+package com.omaryusufonalan.vetappbackend.dto.report;
+
+import com.omaryusufonalan.vetappbackend.dto.appointment.AppointmentUpdateRequest;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ReportUpdateRequest {
+    @Positive
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String diagnosis;
+
+    @PositiveOrZero
+    private double price;
+
+    private AppointmentUpdateRequest appointment;
+}
