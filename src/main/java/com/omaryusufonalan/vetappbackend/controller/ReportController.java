4bd9591caec0fd9791b8da3dfdc80404a024382a
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-03 13:33:40 +0300

    Create ReportController.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/controller/ReportController.java
@@ -0,0 +1,42 @@
+package com.omaryusufonalan.vetappbackend.controller;
+
+import com.omaryusufonalan.vetappbackend.dto.report.ReportCreateRequest;
+import com.omaryusufonalan.vetappbackend.dto.report.ReportUpdateRequest;
+import com.omaryusufonalan.vetappbackend.service.report.ReportService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+import static com.omaryusufonalan.vetappbackend.config.BaseURL.BASE_URL;
+
+@RestController
+@RequestMapping(BASE_URL + "/reports")
+@RequiredArgsConstructor
+public class ReportController {
+    private final ReportService reportService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(reportService.getReportResponseById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody ReportCreateRequest reportSaveRequest) {
+        return new ResponseEntity<>(reportService.createReport(reportSaveRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@Valid @RequestBody ReportUpdateRequest reportUpdateRequest){
+        return new ResponseEntity<>(reportService.updateReport(reportUpdateRequest),HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        reportService.deleteReportById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
