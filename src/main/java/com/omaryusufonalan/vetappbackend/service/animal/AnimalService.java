aae3fb58dddea6b8934ab29b0f29c3b2e5c52bb7
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-04 12:59:26 +0300

    Implement AnimalPage

--- src/main/java/com/omaryusufonalan/vetappbackend/service/animal/AnimalService.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/service/animal/AnimalService.java
@@ -8,13 +8,16 @@ import com.omaryusufonalan.vetappbackend.repository.AnimalRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class AnimalService implements AnimalCRUD, FilterAnimal {
+public class AnimalService implements AnimalCRUD, FilterAnimal, AnimalPage {
     private final AnimalRepository animalRepository;
     private final ModelMapper modelMapper;
 
@@ -58,6 +61,22 @@ public class AnimalService implements AnimalCRUD, FilterAnimal {
     @Override
     public List<AnimalResponse> filterAnimalResponsesByName(String animalName) {
         return animalRepository.findByNameIgnoringCaseContaining(animalName)
+                .stream().map(animal -> modelMapper.map(animal, AnimalResponse.class))
+                .toList();
+    }
+
+    @Override
+    public List<Animal> getAnimalPage(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+
+        Page<Animal> animalPage = animalRepository.findAll(pageable);
+
+        return animalPage.stream().toList();
+    }
+
+    @Override
+    public List<AnimalResponse> getAnimalResponsePage(int page, int pageSize) {
+        return getAnimalPage(page, pageSize)
                 .stream().map(animal -> modelMapper.map(animal, AnimalResponse.class))
                 .toList();
     }
