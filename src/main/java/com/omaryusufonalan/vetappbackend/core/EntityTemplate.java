d44fb6751f4c3eefbaf56f5443c8587820be8a75
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-13 20:42:17 +0300

    Create EntityTemplate.java

---
+++ src/main/java/com/omaryusufonalan/vetappbackend/core/EntityTemplate.java
@@ -0,0 +1,33 @@
+package com.omaryusufonalan.vetappbackend.core;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EntityTemplate {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
