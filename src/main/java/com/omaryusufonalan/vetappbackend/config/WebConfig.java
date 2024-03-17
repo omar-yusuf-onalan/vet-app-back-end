9108953b27d1748014988501e95f7ef37133c66d
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-18 00:00:58 +0300

    Update WebConfig.java

--- src/main/java/com/omaryusufonalan/vetappbackend/config/WebConfig.java
+++ src/main/java/com/omaryusufonalan/vetappbackend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("/**")
+                .allowedOrigins("https://vet-app-front-end.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
